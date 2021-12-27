8452802c62443388f011a20872eaa90282bf5cac
Trey Evans <lewis.r.evans@gmail.com>
2021-12-27 17:38:52 -0500

    Remove sidekiq web for now.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  mount Sidekiq::Web => '/sidekiq'
+  # mount Sidekiq::Web => '/sidekiq'
 
 
   resources :projects, only: [:index]
